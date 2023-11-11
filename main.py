222a73f0749db7f1938836cdad723c12c581181d
ZodD <inzoddwetrust@gmail.com>
2023-11-11 13:40:35 +0400

    Working with any link or Txid

--- main.py
+++ main.py
@@ -2,49 +2,46 @@ import re
 import telebot
 from telebot import types
 
-
-# Replace 'YOUR_BOT_TOKEN' with the token you got from BotFather
 bot = telebot.TeleBot('6951849445:AAG9qk70t3HAZr83xtKJJskHmxBeEX8aE6s')
 
-# Regular expression to match URLs
-url_regex = r'(https?://\S+|www\.\S+)'
+tron_pattern = r'\b([0-9a-fA-F]{64})\b'
+temp_storage = {}
 
 
-@bot.message_handler(func=lambda message: re.search(url_regex, message.text))
+@bot.message_handler(func=lambda message: re.search(tron_pattern, message.text))
 def message_with_link(message):
-    # Display an inline keyboard in the group chat itself
-    markup = types.InlineKeyboardMarkup()
-    urls = re.findall(url_regex, message.text)
-    if urls:  # If any URL is found
-        # Serialize the user ID and the first URL into a string that can be passed in the callback data
-        callback_data = f"save_link:{message.message_id}:{message.from_user.id}:{urls[0]}"
-        if len(callback_data.encode('utf-8')) > 64:  # Telegram allows up to 64 bytes for callback data
-            bot.reply_to(message, 'Error: The link is too long to handle.')
-            return
+    tron_data = re.findall(tron_pattern, message.text)[0]
+    print(tron_data)
 
-        yes_button = types.InlineKeyboardButton('Yes, save it!', callback_data=callback_data)
-        no_button = types.InlineKeyboardButton('No, thanks.', callback_data='do_not_save')
+    if tron_data:
+        # We'll store a tuple of URLs and TXIDs with a unique ID
+        unique_id = str(hash(frozenset(tron_data)))  # A simple unique identifier
+        temp_storage[unique_id] = tron_data
+        print(temp_storage)
+
+        # Send confirmation message with inline keyboard
+        markup = types.InlineKeyboardMarkup()
+        yes_button = types.InlineKeyboardButton('Yes, save it!', callback_data=f'save:{unique_id}')
+        no_button = types.InlineKeyboardButton('No, thanks.', callback_data='ignore')
         markup.add(yes_button, no_button)
-        bot.reply_to(message, 'Do you want to save this link?', reply_markup=markup)
+        bot.reply_to(message, 'Do you want to save this Tron transaction?', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith('save_link'))
-def handle_save_link_callback_query(call):
-    # Extracting the message ID, user ID, and URL from the callback data
-    action, message_id, from_user_id, url = call.data.split(':', 3)
-    message_id = int(message_id)
-    from_user_id = int(from_user_id)
+@bot.callback_query_handler(func=lambda call: call.data.startswith('save:'))
+def handle_save_callback_query(call):
+    unique_id = call.data.split(':')[1]
+    tron_data = temp_storage[unique_id]
 
-    try:
-        # Send only the extracted URL to the user's private chat
-        bot.send_message(from_user_id, f"Here's the link you wanted to save: {url}")
-    except Exception as e:
-        bot.reply_to(call.message,
-                     'I cannot send you the link unless you start a conversation with me. Please message me directly '
-                     'first.')
-        print(e)  # For debugging purposes, you might want to log the exception somewhere instead of printing it
+    if tron_data:
+        try:
+            response_message = f"Here are the Tron transaction details you wanted to save:\n " \
+                               f"https://tronscan.org/#/transaction/{tron_data} \n "
+            bot.send_message(call.from_user.id, response_message)
+        except Exception as e:
+            # Inform the user if the bot cannot send them the message
+            bot.answer_callback_query(call.id, "I can't send you the data. Please make sure you have started a chat with me.")
 
-    bot.answer_callback_query(call.id, text="You chose to save the link.")
+    bot.answer_callback_query(call.id)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'do_not_save')
