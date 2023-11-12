cbc88738b86c8f82fe6a8fb13dbd552ae49c7c23
ZodD <inzoddwetrust@gmail.com>
2023-11-12 09:42:37 +0400

    Add robustness to callback handling with non-persistent storage

--- main.py
+++ main.py
@@ -30,17 +30,27 @@ def message_with_link(message):
 @bot.callback_query_handler(func=lambda call: call.data.startswith('save:'))
 def handle_save_callback_query(call):
     unique_id = call.data.split(':')[1]
-    tron_data = temp_storage[unique_id]
 
-    if tron_data:
+    # Check if the unique_id exists in temp_storage
+    if unique_id in temp_storage:
+        tron_data = temp_storage[unique_id]
+
+        # Send the saved Tron data to the user's private chat
         try:
             response_message = f"Here are the Tron transaction details you wanted to save:\n " \
-                               f"https://tronscan.org/#/transaction/{tron_data} \n "
+                               f"https://apilist.tronscanapi.com/api/transaction-info?hash={tron_data} \n "
             bot.send_message(call.from_user.id, response_message)
+            del temp_storage[unique_id]  # Clean up after sending
         except Exception as e:
             # Inform the user if the bot cannot send them the message
-            bot.answer_callback_query(call.id, "I can't send you the data. Please make sure you have started a chat with me.")
+            bot.answer_callback_query(call.id,
+                                      "I can't send you the data. Please make sure you have started a chat with me.")
+            print(e)  # For debugging purposes
+    else:
+        # Inform the user that the data is no longer available
+        bot.answer_callback_query(call.id, "The data you are trying to save is no longer available.")
 
+    # This call should be outside the 'if' block to ensure it's always executed.
     bot.answer_callback_query(call.id)
 
 
